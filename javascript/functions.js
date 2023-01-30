76c0fc968ff8d142c2b07bbe9af6b6ec2848b5c8
mateobart <123991277+mateobart@users.noreply.github.com>
2023-01-30 17:23:25 -0300

    Add files via upload

---
+++ javascript/functions.js
@@ -0,0 +1,42 @@
+document.getElementById("contacto_boton").addEventListener("click", function(){
+    if(document.getElementById("contacto_box").style.display === "none"){
+        document.getElementById("contacto_box").style.display = "block";
+    } else {
+        document.getElementById("contacto_box").style.display = "none";
+    }
+})
+
+document.getElementById("resumen_boton").addEventListener("click", function(){
+    if(document.getElementById("resumen_box").style.display === "none"){
+        document.getElementById("resumen_box").style.display = "block";
+    } else {
+        document.getElementById("resumen_box").style.display = "none";
+    }
+})
+
+
+document.getElementById("habilidades_boton").addEventListener("click", function(){
+    if(document.getElementById("habilidades_box").style.display === "none"){
+        document.getElementById("habilidades_box").style.display = "block";
+    } else {
+        document.getElementById("habilidades_box").style.display = "none";
+    }
+})
+
+
+document.getElementById("experiencia_boton").addEventListener("click", function(){
+    if(document.getElementById("experiencia_box").style.display === "none"){
+        document.getElementById("experiencia_box").style.display = "block";
+    } else {
+        document.getElementById("experiencia_box").style.display = "none";
+    }
+})
+
+
+document.getElementById("educacion_boton").addEventListener("click", function(){
+    if(document.getElementById("educacion_box").style.display === "none"){
+        document.getElementById("educacion_box").style.display = "block";
+    } else {
+        document.getElementById("educacion_box").style.display = "none";
+    }
+})
